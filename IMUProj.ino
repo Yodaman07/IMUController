ce0ef0578c117f715bfd2024bbb6ea6d61383e5f
Yodaman07 <ayaan.irshad@gmail.com>
2026-05-16 14:51:57 -0700

    started working on other axis - got y and z working together

--- IMUProj.ino
+++ IMUProj.ino
@@ -4,7 +4,7 @@
 #include "USBHIDKeyboard.h"
 //https://github.com/espressif/arduino-esp32/blob/master/libraries/USB/src/USBHIDKeyboard.h
 
-
+//TODO: REFACTOR FOR CLEAN CODE
 #define green_pin 2
 #define blue_pin 3
 
@@ -14,9 +14,16 @@ Adafruit_ISM330DHCX ism330dhcx;
 
 int green_state = 0;
 int blue_state = 0;
-float gyro_angle = 0;
 double old_time = 0;
-double blender_angle = 0;
+
+double gyro_z = 0;
+double blender_z = 0;
+
+double gyro_x = 0;
+double blender_x = 0;
+
+double gyro_y = 0;
+double blender_y = 0;
 
 bool hold_condition = false; //if true then sys will work
 
@@ -53,20 +60,35 @@ void loop() {
   double dt = (current_time - old_time) / 1000;
   old_time = current_time;
 
-  gyro_angle += (gyro.gyro.z * dt) * (180/PI);
+  gyro_z += (gyro.gyro.z * dt) * (180/PI);
+  gyro_x += (gyro.gyro.x * dt) * (180/PI);
+  gyro_y += (gyro.gyro.y * dt) * (180/PI);
+
   Serial.print("Gyro Z Angle: ");
-  Serial.println(gyro_angle);
+  Serial.println(gyro_z);
+  Serial.print("Gyro X Angle: ");
+  Serial.println(gyro_x);
+  Serial.print("Gyro Y Angle: ");
+  Serial.println(gyro_y);
+
   Serial.print(green_state);
   Serial.print("     ");
   Serial.print(blue_state);
   Serial.println();
-  if (hold_condition){
-    if ((gyro_angle-blender_angle) >= 15){ //AS LONG AS COLLECTION IS CONTINUOUS MEANING HOLD CONDITOIN IS ALWAYS TRUE: mthe gyro won't really ever have an angle change of more than 15˚ because of how frequently it collects data
-      ccw_15();
-    }
-    else if ((gyro_angle-blender_angle)<=-15){
-      cw_15();
-    }
+
+
+  if (hold_condition){ //Can comment to enable/disable axis
+     //AS LONG AS COLLECTION IS CONTINUOUS MEANING HOLD CONDITOIN IS ALWAYS TRUE: mthe gyro won't really ever have an angle change of more than 15˚ because of how frequently it collects data
+    if ((gyro_z-blender_z) >= 15){ccw_z();}
+    else if ((gyro_z-blender_z)<=-15){cw_z();}
+
+
+    if ((gyro_y-blender_y) >= 15){cw_y();}
+    else if ((gyro_y-blender_y)<=-15){ccw_y();}
+
+    
+    // if ((gyro_x-blender_x) >= 15){ccw_x();}
+    // else if ((gyro_x-blender_x)<=-15){cw_x();}
   }
 
 
@@ -74,26 +96,45 @@ void loop() {
   blue_state = digitalRead(blue_pin);
 
   //for some reason the var names don't work so i put the hex codes from here https://github.com/espressif/arduino-esp32/blob/master/libraries/USB/src/USBHIDKeyboard.h
-  if (green_state == HIGH){ //resets gyro angle
-    gyro_angle = 0;
-    blender_angle = 0;
-  }
-
+  if (green_state == HIGH){ reset_all();}
   if (blue_state == HIGH){hold_condition = true;}
   else{hold_condition = false;}
-  delay(100);
+  delay(100); //prob wanna use milis instead
 }
 
-void cw_15(){ //move 15˚ cw
-    Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.press(0xE6); //moves right
-    Keyboard.releaseAll();
-    blender_angle-=15;
-}
-
-void ccw_15(){ //move 15˚ ccw
+void cw_z(){ //move 15˚ cw
   Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.press(0xE4); //moves left
-    Keyboard.releaseAll();
-    blender_angle+=15;
+  Keyboard.press(0xE6); //roll right
+  Keyboard.releaseAll();
+  blender_z-=15;
+}
+
+void ccw_z(){ //move 15˚ ccw
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(0xE4); //roll left
+  Keyboard.releaseAll();
+  blender_z+=15;
+}
+
+void cw_y(){ //move 15˚ forward/cw
+  Keyboard.press(0xE8); //orbit up
+  Keyboard.releaseAll();
+  blender_x+=15; //CHECK
+}
+
+void ccw_y(){ //move 15˚ backward/ccw
+  Keyboard.press(0xE2); //orbit down
+  Keyboard.releaseAll();
+  blender_x-=15;
+}
+
+void reset_all(){
+  gyro_z = 0;
+  blender_z = 0;
+
+  gyro_x = 0;
+  blender_x = 0;
+
+  gyro_y = 0;
+  blender_y = 0;
 }
